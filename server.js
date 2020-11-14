0eccd7de2da5b94b5db5200edb636ec3f4fdb1d6
DESKTOP-A19MMGA\HoangVinh <hoang312tnhn@gmail.com>
2020-11-14 16:21:41 +0700

    make it better

--- server.js
+++ server.js
@@ -1,54 +1,76 @@
+//Su dung framework expressjs
 const express=require('express');
 const app=express();
 const path=require('path');
 
+//Cai dat thu muc tinh css,js,image...
 app.use(express.static(path.join(__dirname,'src/public')));
 
 const server=require('http').Server(app);
+
+//Su dung socket 
 const io=require('socket.io')(server);
 
-
+//Su dung express-handlebars 
 var exphbs  = require('express-handlebars');
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'src/views'));
 
+//Them bo doc giai ma cac tep 
 app.use(express.urlencoded({
 	extended:true}));
 app.use(express.json());
+
+//Server lang nghe port 3000 hoac port tu cong moi truong khac
 server.listen(process.env.PORT||3000,function(){
 	console.log('Da ket noi');
 
 });
 /////////////////////////////////////////////////////
-var list=[""];
-var listId=[""];
+var list=[""];//Danh sach nguoi dang nhap
+var listId=[""];//Danh sach socketId nguoi dang nhap
+
+//Xu li du lieu khi co nguoi dang nhap
 io.on("connection",function(socket){
 	console.log(socket.id);
 	listId.push(socket.id);
-	console.log('Co nguoi ket noi');
+	console.log('Co nguoi ket noi');//Thog bao co nguoi ket noi
+
+	//Neu danh sach co nguoi thi gui toan bo danh sach cho nguoi moi vao
 	if(list.length>1)socket.emit('list',list);
 
-	socket.on("client-send",function(data){
-		console.log(data);
-		socket.broadcast.emit("server-send",data);
-	})
+	//Them nguoi vao he thong neu nhu ten dang nhap khong trung voi ten nguoi da dang ky
 	socket.on("user-name",function(data){
 		var ok=true;
 		for(var i=0;i<list.length;i++){
+			//Kiem tra xem co trung ten hay khong
 			if(data==list[i]){
 				console.log('trung ten');
 				ok=false;
 				break;
 			}
 		}
+		//Ten hop le them vao danh sach
 		if(ok){
 			list.push(data);	
-			io.sockets.emit("confirm-name",data);
-			socket.emit("name",data);
+			socket.broadcast.emit("confirm-name",data);//Gui ten den tat ca nguoi dung online
+			socket.emit("name",data);//Gui lai ten cho chinh nguoi dung
+		}
+		//Ten khong hop le
+		else{
+			console.log("error");
+			socket.emit("Erorr",ok)
 		} 
 		console.log(list);
 	})
+	//Gui tin nhan cho nhung nguoi cung he thong
+	socket.on("client-send",function(data){
+		console.log(data);
+		socket.broadcast.emit("server-send",data);
+	})
+
+	//Xu ly nhung nguoi thoat khoi app
 	socket.on('disconnect',function(){
 		for(var i=1;i<listId.length;i++){
 			if(listId[i]==socket.id){
@@ -61,7 +83,8 @@ io.on("connection",function(socket){
 		console.log(list);
 	})
 })
-/////////////////////////////////////////////////////////
+
+//Tra ve tranh home
 app.get('/',function(req,res){
 	res.render('home');
 })
--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,7 +1,9 @@
-const socket = io('https://chatbot-duan1.herokuapp.com');
+
+const socket = io('https://chatbot-duan1.herokuapp.com');   //Xu dung socket de bat su kien 
+
 var news=0;
 var newsMasage=document.getElementById('newsMasage');
-
+//Xu lu dong mo chat
 function openForm() {
   document.getElementById("myForm").style.display = "block";
   news=0;
@@ -12,19 +14,29 @@ function closeForm() {
   news=0;
 }
 
-/////////////////////////////////////////////////////////
-var uname=document.getElementById('uname');
+//Xu ly noi dung chat
+var uname=document.getElementById('uname');                 //Lay ten nguoi dung
+
 var button=document.getElementById('button-login');
-var myUL=document.getElementById('myUL');
+
+var myUL=document.getElementById('myUL');                   //Khung hien tin nhan
+
+//Gui ten nguoi dung muon dang ky ve he thong
 button.addEventListener('click',function(){
     socket.emit("user-name",uname.value);
 })
-var user_name=document.getElementById('user-name');
+
+//Xu ly ten nguoi dung
+var user_name=document.getElementById('user-name');         //Lay the co chua ten nguoi dung
+
+    //Xu ly ten nguoi dung khi server chap nhan ten hop le
     socket.on('confirm-name',function(data){
-    var newPerson=document.createElement('li');
-    newPerson.innerHTML="<a>" +data+ "</a>";
-    myUL.appendChild(newPerson);
+    var newPerson=document.createElement('li');             //Tao them mot the de chua ten nguoi dung
+    newPerson.innerHTML="<a>" +data+ "</a>";                //Gan ten nguoi dung vao the
+    myUL.appendChild(newPerson);                            //Them vao danh sach nguoi dang online
 })
+
+//Hien thi danh sach nhung nguoi dang online     
 socket.on('list',function(data){
     for(var i=1;i<data.length;i++){
         var newPerson=document.createElement('li');
@@ -33,11 +45,14 @@ socket.on('list',function(data){
     }
     
 })
+
+//Hien thi ten nguoi dung 
 socket.on('name',function(data){
     user_name.innerHTML="<b>"+data+"</b>";
     document.getElementById('id01').style.display="none";
 })
-/////////////////////////////////////////////////////////
+
+//Xu ly khi co nguoi thoat khoi app
 socket.on('remove',function(data){
     var List=myUL.children;
     for(var i=0;i<List.length;i++){
@@ -46,13 +61,23 @@ socket.on('remove',function(data){
         }
     }
 })
-///////////////////////////////////////////////////////////////////////
-var send=document.getElementById('send');
-var message=document.getElementById('message-input');
-var request=document.getElementById('message-out');
+
+//Xu ly tin nhan
+var send=document.getElementById('send');//Nut gui
+var message=document.getElementById('message-input');//Tin nhan nguoi dung nhap
+var request=document.getElementById('message-out');//Hien thi tin nhan
+
+//Xu ly su kien nhan nut send
 send.addEventListener('click',function(){
+
+    //Kiem tra xem ti nhan co rong khong
     if(message.value!=""){
+
+        //Gui tin nhan len server
         socket.emit("client-send", "<b>"+user_name.textContent+": "+"</b>" +message.value);
+        
+        //Hien thi tin nhan len khung
+
         var rq=document.createElement('div');
         var clear=document.createElement('div');
         clear.classList.add('clear');
@@ -61,31 +86,32 @@ send.addEventListener('click',function(){
         request.appendChild(rq);
         request.appendChild(clear);
         message.value="";
-        request.scrollTop=request.scrollHeight;
+        request.scrollTop=request.scrollHeight;//Chinh con chuot lan hien thi tai tin nhan vua moi gui
     }
 })
-//////////////////////////////////////////////////////////////
+
+//Xu ly tin nhan khi nhan duoc nhung nguoi dung khac
 socket.on("server-send",function(data){
     news+=1;
     if(news!=0){
     newsMasage.textContent=" ("+news+")";
-}
+    }
 	var rs=document.createElement('div');
 	var clear=document.createElement('div');
     
 	clear.classList.add('clear');
 	rs.innerHTML="<span>"+data+"</span>";
     rs.classList.add('textLeft');
-	request.appendChild(rs);
+	request.appendChild(rs);//Hien thi tin nhan nhung nguoi khac
 	request.appendChild(clear);
-    request.scrollTop=request.scrollHeight;
+    request.scrollTop=request.scrollHeight;//Chinh con chuot lan hien thi den vi tri tin nhan moi nhat
 	console.log(data);
 })
 //////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////
+//Xu ly tim kiem ten trong danh sach nguoi dung dang online
 function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
@@ -102,3 +128,9 @@ function myFunction() {
         }
     }
 }
+//////////////////////////////////
+socket.on("Error",function(data){
+   if(!data){
+    alert("Ten dang nhap da co nguoi dung");
+   }
+});
